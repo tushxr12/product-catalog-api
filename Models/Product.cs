d5d5adf3eba4c3a14fc4a91d71c95e6640cdcbc6
Tushar <tusharforcourses@gmail.com>
2025-12-12 00:35:40 +0530

    make field required in model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,16 +1,21 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace HPlusSport.API.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        [Required]
         public string Sku { get; set; } = string.Empty;
+        [Required]
         public string Description { get; set; } = string.Empty;
         public bool IsAvailable { get; set; }
 
+        [Required]
         public int CategoryId { get; set; }
 
         [JsonIgnore]
